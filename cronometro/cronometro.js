9775c106d466de3524b28bb403222f2a0a622c42
mairtonr <mairtonhotmailcomr44@gmail.com>
2022-05-09 16:18:05 -0300

    Atualizar Calculadora

---
+++ cronometro/cronometro.js
@@ -0,0 +1,24 @@
+var sec = 00
+var min =0
+var interval
+function start () {
+    interval= setInterval(contador, 1000)
+}
+
+function pause() {
+   clearInterval(interval) 
+}
+
+function stop() {
+   clearInterval(interval)
+   sec=0
+   min=0
+   document.getElementById('contador').innerHTML='00:00'
+}
+function contador() {
+ sec++
+ if (sec==60)
+    min++
+    sec=0
+ document.getElementById('contado').innerHTML=min+':'+sec   
+}
