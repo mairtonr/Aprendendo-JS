d9ad59a17fdb7a087b5903b20b4e7afee5c5d91a
mairtonr <mairtonhotmailcomr44@gmail.com>
2022-05-15 15:06:34 -0300

    Lâmpada

---
+++ lâmpada/lampada.js
@@ -0,0 +1,31 @@
+const acender = document.getElementById('on')
+const apagar = document.getElementById('off')
+const lamp = document.getElementById('dlg')
+
+function broken() {
+    return dlg.src.indexOf('quebrada') > -1
+}
+
+function lampOn() {
+    if (!broken()) {
+        lamp.src = './img/ligada.jpg'
+    }
+
+}
+
+function lampOff() {
+    if (!broken()) {
+        lamp.src = './img/desligada.jpg'
+    }
+
+}
+
+function lampQ() {
+    lampQ.src = './img/quebrada.jpg'
+}
+
+acender.addEventListener('click', lampOn)
+apagar.addEventListener('click', lampOff)
+lamp.addEventListener('mouseover', lampOn)
+lamp.addEventListener('mouseleave', lampOff)
+lamp.addEventListener('dbclick', lampQ)
