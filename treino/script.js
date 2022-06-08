f56b50f07a6ba8e1f059bc5a9e4bb1aca0a35eba
mairtonr <mairtonhotmailcomr44@gmail.com>
2022-06-08 08:48:08 -0300

    Troca de Cor com div

---
+++ treino/script.js
@@ -0,0 +1,8 @@
+    let mudarcor = document.querySelector('.container')
+    mudarcor.addEventListener('click', ()=>{
+      mudarcor.style.background = "gray"
+    })
+    
+    mudarcor.addEventListener('mouseout', ()=>{
+        mudarcor.style.background = ""
+    })
